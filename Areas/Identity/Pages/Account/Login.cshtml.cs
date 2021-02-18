e221683519b6531eec04f803f4c0578b31f3b137
jpalomino1991 <palomino.j@pucp.edu.pe>
2021-02-17 22:14:10 -0500

    change in language on login

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -99,7 +99,7 @@ namespace ShopifyWeb.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Clave o usuario incorrecto.");
                     return Page();
                 }
             }
