582d3c9d0e345602b423ca7138e56b9a421fdfb8
jpalomino1991 <palomino.j@pucp.edu.pe>
2021-02-25 22:10:04 -0500

    added new field in product download

--- Models/ProductDownload.cs
+++ Models/ProductDownload.cs
@@ -36,9 +36,10 @@ namespace ShopifyWeb.Models
         public string MaterialSuela { get; set; }
         public string HechoEn { get; set; }
         public string CodigoProducto { get; set; }
+        public int Peso { get; set; }
         public string ToLine()
         {
-            return $"{SKU};{Handle};{Title};{Tags};{Id};{ProductType};{Vendor};{SEOTitle};{SEODescription};{URL};{Talla};{Stock};{PrecioTV};{PrecioPromocion};{Color};{Material};{Taco};{Genero};{Ocasion};{Tendencia};{Departamento};{Status};{CantidadImagenes};{CreateDate};{Marca};{MaterialInterior};{MaterialSuela};{HechoEn};{CodigoProducto};";
+            return $"{SKU};{Handle};{Title};{Tags};{Id};{ProductType};{Vendor};{SEOTitle};{SEODescription};{URL};{Talla};{Stock};{PrecioTV};{PrecioPromocion};{Color};{Material};{Taco};{Genero};{Ocasion};{Tendencia};{Departamento};{Status};{CantidadImagenes};{CreateDate};{Marca};{MaterialInterior};{MaterialSuela};{HechoEn};{CodigoProducto};{Peso};";
         }
     }
 }
